13af34c3931177ad5445f8ab7ba2ad77176aa00b
Raymon de Looff <raydelooff@gmail.com>
2018-05-05 18:55:47 +0200

    Update Vagrant setup

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -1,36 +1,52 @@
-#!/bin/sh
+#!/bin/bash
 
-WEEWX_VERSION="3.5.0"
+PYTHON_VERSION="2.7.15"
+WEEWX_VERSION="3.8.0"
 
-# Install required packages
-sudo apt-get -y install python-configobj python-cheetah python-imaging python-pip
+echo '
+export LC_ALL=C' >> ~/.bashrc
 
-# Install Pusher
-pip install pusher
+# Update packages and install build packages
+# https://github.com/pyenv/pyenv/wiki/Common-build-problems
+sudo apt-get update
+sudo apt-get dist-upgrade
 
-# Build WeeWX
-cd /home/vagrant
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev
+
+# Install pyenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+echo '
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+
+pyenv install $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+
+# Install dependencies
+pip install --upgrade pip
+pip install configobj Cheetah pillow pusher
+
+# Build and install WeeWX
 wget http://weewx.com/downloads/weewx-${WEEWX_VERSION}.tar.gz
 tar xvfz weewx-${WEEWX_VERSION}.tar.gz
 cd weewx-${WEEWX_VERSION}
 ./setup.py build
 
-# Install WeeWX
-cd /home/vagrant
-cd weewx-${WEEWX_VERSION}
-sudo ./setup.py install --no-prompt
+sed -i -e 's/\/home\/weewx/\/home\/vagrant/g' setup.cfg
+./setup.py install --no-prompt
 
-# Run WeeWX as a daemon
-cd /home/weewx
-sudo cp util/init.d/weewx.debian /etc/init.d/weewx
-sudo chmod +x /etc/init.d/weewx
-sudo update-rc.d weewx defaults 98
+cd ~
+rm -r weewx-${WEEWX_VERSION}*
 
-# Move configuration
-rm /home/weewx/weewx.conf
-mv /home/vagrant/weewx.conf /home/weewx/weewx.conf
+echo '
+export PATH="~/bin:$PATH"' >> ~/.bashrc
 
-# Start WeeWX
-sudo /etc/init.d/weewx start
-
-
+# Link project
+ln -s /home/vagrant/weewx-pusher/bin/user/pusher_.py /home/vagrant/bin/user/pusher_.py
--- Vagrantfile
+++ Vagrantfile
@@ -3,13 +3,12 @@
 
 Vagrant.configure("2") do |config|
 	# Box
-	config.vm.box = "ubuntu/trusty64"
+	config.vm.box = "ubuntu/bionic64"
 
 	# Share extension
-	config.vm.synced_folder ".", "/home/vagrant/extension"
-	config.vm.synced_folder "./bin/user/pusher", "/home/weewx/bin/user/pusher"
+	config.vm.synced_folder "./bin/user", "/home/vagrant/weewx-pusher/bin/user"
 
 	# Provision machine
 	config.vm.provision "file", source: "./.provision/weewx.conf", destination: "/home/vagrant/weewx.conf"
-	config.vm.provision "shell", path: "./.provision/bootstrap.sh"
+	config.vm.provision "shell", path: "./.provision/bootstrap.sh", privileged: false
 end
