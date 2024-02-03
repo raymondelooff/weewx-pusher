a39a09d6a69f882011ffa7dd83b03c887fac2498
Raymon de Looff <raydelooff@gmail.com>
2024-02-03 18:42:09 +0100

    Update copyright years

--- bin/user/wxpusher.py
+++ bin/user/wxpusher.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2016 Raymon de Looff <raydelooff@gmail.com>
+# Copyright (c) 2016-2024 Raymon de Looff <raydelooff@gmail.com>
 # This extension is open-source software licensed under the GPLv3 license.
 
 __version__ = '2.0.0-dev'
